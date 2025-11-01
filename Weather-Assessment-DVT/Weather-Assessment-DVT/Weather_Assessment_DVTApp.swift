a1b954a141e0b10b2efa5469609fe93207def36a
ajeet2012 <ajeetmca2012@gmail.com>
2025-11-02 00:11:31 +0400

    chore: initial project create

---
+++ Weather-Assessment-DVT/Weather-Assessment-DVT/Weather_Assessment_DVTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Assessment_DVTApp.swift
+//  Weather-Assessment-DVT
+//
+//  Created by Ajeet Sharma on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Assessment_DVTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
